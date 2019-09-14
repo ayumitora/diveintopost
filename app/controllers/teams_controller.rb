5b609384003d9027ba4447cfb0e219a48291f579
koda <hogehogehoge@gmail.com>
2019-09-14 17:22:36 +0900

    teamリーダーが編集しようとすると、root_pathにリダイレクトされるように変更

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,11 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit
+    unless current_user.id == @team.owner_id
+      redirect_to root_path, notice: '権限がありません'
+    end
+  end
 
   def create
     @team = Team.new(team_params)
